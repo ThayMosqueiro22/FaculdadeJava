b29a202d5ecddcc636dd2116eaf2d2d71852ef21
Thaiany <thaianyemilydonascimentomosque@gmail.com>
2026-03-16 18:16:11 +0000

    Java inicial

---
+++ Produto.java
@@ -0,0 +1,34 @@
+public class Produto {
+
+    String nome;
+    double preco;
+
+    public Produto(String nome, double preco){
+
+        this.nome = nome;
+        this.preco = preco;
+
+    }
+
+    public Produto (String nome){
+
+        this.nome = nome;
+        this.preco = 10.0;
+
+    }
+
+    public void exibirDetalhes() {
+
+        System.out.println(" Produto: " + nome + " Preço; R$: " + preco );
+        
+    }
+
+    public static void main(String[] args) {
+        Produto p1 = new Produto ("Caneta", 3.5);
+        Produto p2 = new Produto ("Lapis");
+
+        p1.exibirDetalhes();
+        p2.exibirDetalhes();
+    }
+}
+
